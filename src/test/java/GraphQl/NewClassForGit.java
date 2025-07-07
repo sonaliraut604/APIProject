0d80410f6c299f0beaf12aac157a7624fe19a0c7
sonali <sonaliraut5151@gmail.com>
2025-07-07 18:52:44 +0530

    develope branch change

--- src/test/java/GraphQl/NewClassForGit.java
+++ src/test/java/GraphQl/NewClassForGit.java
@@ -4,25 +4,27 @@ public class NewClassForGit {
 
 	
 		
-		public void gitmethod()
-		{
-			System.out.println("i am on the git downloaded person repo");
-			System.out.println("new command");
-		}
-		
-		
-		public void setmethod()
-		{
-			System.out.println("this is first user who write code");
-			System.out.println("add");
-		}
-		
-		public void method()
-		{
-			
-			System.out.println("third time");
-		}
-		
+	public void gitmethod()
+	{
+		System.out.println("i am on the git downloaded person repo");
+	}
+	
+	public void set()
+	{
+		System.out.println("i am owner of this");
+	}
+	
+
+	public void mobile()
+	{
+		System.out.println("i have mobile");
+	}
+
+	public void iphome()
+	{
+		System.out.println("i have iphone");
+	}
+	
 		
 	}
 

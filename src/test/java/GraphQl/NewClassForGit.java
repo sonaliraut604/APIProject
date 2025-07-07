00f5fb9b137d1ffee8ab262c1363e67a4266c6d5
sonali <sonaliraut5151@gmail.com>
2025-07-07 18:17:53 +0530

    new first

--- src/test/java/GraphQl/NewClassForGit.java
+++ src/test/java/GraphQl/NewClassForGit.java
@@ -7,6 +7,14 @@ public class NewClassForGit {
 		public void gitmethod()
 		{
 			System.out.println("i am on the git downloaded person repo");
+			System.out.println("new command");
+		}
+		
+		
+		public void setmethod()
+		{
+			System.out.println("this is first user who write code");
+			System.out.println("add");
 		}
 		
 		
